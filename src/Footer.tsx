4ab3d2b2ba330ec3cabd78f8306ddd1a676e2683
imlouislab <louisamorosbessede@gmail.com>
2023-02-28 16:25:41 +0100

    add footer and github information

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,12 +3,24 @@ import { AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="flex flex-row justify-center items-center gap-2">
-        <a href="/">
-          <AiFillGithub />
-        </a>
-        <a href="/">Github</a>
+    <div className="fixed bottom-0 pt-3 w-screen mx-auto">
+      <div className="flex flex-row items-center align-center justify-between w-5/6 mx-auto mb-5">
+        <div>
+          <p className="text-white text-l">
+            This project is open-sourced and open to contributions!
+          </p>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-2">
+          <a
+            href="https://github.com/iamlouislab/supabase-types-converter"
+            target="_blank"
+          >
+            <div className="flex flex-row text-white text-xl align-center items-center gap-2">
+              <AiFillGithub />
+              Github
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
